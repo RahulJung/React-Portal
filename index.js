5b920b26227f80dc6bfa910070758c70fc5ed68a
RahulJung <rahuljungchauhan@gmail.com>
2020-11-28 14:00:53 -0500

    Create file structure for webpack

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const port = 8080;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Helloworld');
+app.get("/", (req, res) => {
+  res.send("Helloworld hehehe");
 });
 
 app.listen(port, () => {
-    console.log(`Connected to port http://localhost:${port}}`)
-})
+  console.log(`Connected to port http://localhost:${port}`);
+});
